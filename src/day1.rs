ea4b89adb25a4d588393b331bfbd47723daddb53
Felix Kaaman <tmtu@tmtu.ee>
2022-12-04 00:34:17 +0100

    Slightly faster day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -19,3 +19,26 @@ pub fn part2(input: &str) -> u32 {
     elf_calories.sort_unstable();
     elf_calories.iter().rev().take(3).sum()
 }
+
+
+#[aoc(day1, part2, Faster)]
+pub fn part2_opt(input: &str) -> u32 {
+    let elf_calories = input
+        .split("\n\n")
+        .map(|c| c.lines().map(|l| l.parse::<u32>().unwrap()).sum());
+
+    let mut top = [0, 0, 0];
+
+    for calories in elf_calories {
+        if calories > top[0] {
+            top = [calories, top[0], top[1]];
+        } else if calories > top[1] {
+            top[2] = top[1];
+            top[1] = calories;
+        } else if calories > top[2] {
+            top[2] = calories;
+        }
+    }
+
+    top.iter().sum()
+}
